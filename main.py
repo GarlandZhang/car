f4fadba918996fec8703d38699ac09fab1fd0dfc
GarlandZhang <garland._zhang@hotmail.com>
2019-10-06 01:56:10 -0400

    Add serialized Facenet model

--- main.py
+++ main.py
@@ -24,15 +24,16 @@ import sys
 np.set_printoptions(threshold=sys.maxsize)
 
 FRmodel = faceRecoModel(input_shape=(3,96,96))
-
-with tf.Session() as test:
-  tf.set_random_seed(1)
-  y_true = (None, None, None)
-  y_pred = (tf.random_normal([3, 128], mean=6, stddev=0.1, seed=1),
-            tf.random_normal([3, 128], mean=1, stddev=1, seed=1),
-            tf.random_normal([3, 128], mean=3, stddev=4, seed=1))
-  loss = triplet_loss(y_true, y_pred)
-  print("loss = " + str(loss.eval()))
+FRmodel._make_predict_function()
+graph = tf.get_default_graph()
+# with tf.Session() as test:
+#   tf.set_random_seed(1)
+#   y_true = (None, None, None)
+#   y_pred = (tf.random_normal([3, 128], mean=6, stddev=0.1, seed=1),
+#             tf.random_normal([3, 128], mean=1, stddev=1, seed=1),
+#             tf.random_normal([3, 128], mean=3, stddev=4, seed=1))
+#   loss = triplet_loss(y_true, y_pred)
+#   print("loss = " + str(loss.eval()))
 
 FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
 load_weights_from_FaceNet(FRmodel)
